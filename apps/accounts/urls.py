ee9a18db8b8dcef87e06d132784fb599a9f8807e
nguyenthetai123 <nguyenthetai.itdev97@gmail.com>
2023-12-30 21:45:53 +0700

    userprofile api + changepassword api

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import  path
 from . import views
 urlpatterns=[
     path('register', views.Registerview.as_view(), name='register'),
-    path('login',views.LoginView.as_view(),name='login')
+    path('login',views.LoginView.as_view(),name='login'),
+    path('profile',views.UserProfile.as_view(),name='profile'),
+    path('changepassword', views.ChangePasswordUser.as_view(),name='change')
 ]
